0c99650c1cc5b8d0ffd6d2802ac91b8c159121ed
NGUYỄN ANH HÀO - ITC - Finance - IT Trainee <hao.nguyen2@mservice.com.vn>
2020-12-22 10:52:51 +0700

    refactor: exception definition

--- src/main/java/exception/ValidateException.java
+++ src/main/java/exception/ValidateException.java
@@ -3,6 +3,20 @@ package exception;
 import java.lang.reflect.Field;
 
 public abstract class ValidateException extends Exception {
-    Field errorField;
-    String message;
+    protected Field errorField;
+    protected String message;
+
+    protected ValidateException(Field errorField, String message){
+        this.errorField = errorField;
+        this.message = message;
+    }
+
+    public Field getErrorField() {
+        return errorField;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
 }
