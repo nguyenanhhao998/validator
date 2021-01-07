dc43165d8884c2c9b16166927b480689e112c3ff
NGUYỄN ANH HÀO - ITC - Finance - IT Trainee <hao.nguyen2@mservice.com.vn>
2021-01-08 01:00:46 +0700

    Turn off java reflective warning, refactor validator.

---
+++ src/main/java/com/d3h/validation/util/WarningUtils.java
@@ -0,0 +1,25 @@
+package com.d3h.validation.util;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class WarningUtils {
+    @SuppressWarnings("unchecked")
+    public static void disableAccessWarnings() {
+        try {
+            Class unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field field = unsafeClass.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            Object unsafe = field.get(null);
+
+            Method putObjectVolatile = unsafeClass.getDeclaredMethod("putObjectVolatile", Object.class, long.class, Object.class);
+            Method staticFieldOffset = unsafeClass.getDeclaredMethod("staticFieldOffset", Field.class);
+
+            Class loggerClass = Class.forName("jdk.internal.module.IllegalAccessLogger");
+            Field loggerField = loggerClass.getDeclaredField("logger");
+            Long offset = (Long) staticFieldOffset.invoke(unsafe, loggerField);
+            putObjectVolatile.invoke(unsafe, loggerClass, offset, null);
+        } catch (Exception ignored) {
+        }
+    }
+}
